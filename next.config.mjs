0eee6e3efe75db3c36f6a157afa487bc7f60e2d2
DavidChagas <36827231+DavidChagas@users.noreply.github.com>
2024-09-29 13:49:10 -0300

    fix: local isso quebrou

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,4 @@
 /** @type {import('next').NextConfig} */
-
-const nextConfig = {
-  basePath: '/eecode', // O caminho base, para que os links e assets estejam corretos no GitHub Pages
-  trailingSlash: true, // Garante que todas as páginas exportadas terminem com '/'
-  images: {
-    unoptimized: true, // Desabilita a otimização de imagens (necessário para exportação estática)
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
