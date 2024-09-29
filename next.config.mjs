0eef52e74a83cc9c6c119f243357bc8e7e157156
DavidChagas <36827231+DavidChagas@users.noreply.github.com>
2024-09-29 13:36:18 -0300

    configs para deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const nextConfig = {
+  basePath: '/eecode', // O caminho base, para que os links e assets estejam corretos no GitHub Pages
+  trailingSlash: true, // Garante que todas as páginas exportadas terminem com '/'
+  images: {
+    unoptimized: true, // Desabilita a otimização de imagens (necessário para exportação estática)
+  },
+};
 
 export default nextConfig;
