4c12c43668acef5daef8c22924361e9c414aea8b
DavidChagas <36827231+DavidChagas@users.noreply.github.com>
2024-09-29 23:18:53 -0300

    teste

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = {
-  basePath: isProd ? '/eecode' : '', // Use o basePath apenas em produção
-  trailingSlash: true, // Garante que as URLs terminem com "/"
+  output: 'export',
+  trailingSlash: true, // Isso garante que as rotas terminem com "/" no export estático
+  basePath: isProd ? '/eecode' : '', // Ajuste para o nome do subdiretório, se necessário
+  assetPrefix: isProd ? '/eecode/' : '',
   images: {
-    unoptimized: true, // Necessário para export estático
+    unoptimized: true, // Necessário para export estático no GitHub Pages
   },
 };
