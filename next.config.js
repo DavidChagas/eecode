710c2a945d1bfd934f3152a82e6a837458db9573
DavidChagas <36827231+DavidChagas@users.noreply.github.com>
2024-09-29 22:46:52 -0300

    Revert "fix: ajuste next config"
    This reverts commit 294634f764f028b549e3aba3b775e4d9bb458897.

--- next.config.js
+++
@@ -1,9 +0,0 @@
-const isProd = process.env.NODE_ENV === 'production';
-
-module.exports = {
-  basePath: isProd ? '/eecode' : '', // Use o basePath apenas em produção
-  trailingSlash: true, // Garante que as URLs terminem com "/"
-  images: {
-    unoptimized: true, // Necessário para export estático
-  },
-};
