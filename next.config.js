94c0b843f3ae7d79740faba8e2f9ec3e31201864
DavidChagas <36827231+DavidChagas@users.noreply.github.com>
2024-10-03 22:49:46 -0300

    teste se isso é necessario

--- next.config.js
+++ next.config.js
@@ -1,9 +1,4 @@
-module.exports = {
-  output: 'export',
-  trailingSlash: true, // Isso garante que as rotas terminem com "/" no export estático
-  basePath:'', // Ajuste para o nome do subdiretório, se necessário
-  assetPrefix: '',
-  images: {
-    unoptimized: true, // Necessário para export estático no GitHub Pages
-  },
-};
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
+
+export default nextConfig;
